e2f7aa4c7faef2080271a9177d9346f8aaebae6b
Renan Castro <renandf@gmail.com>
2020-09-13 09:39:26 +1000

    installed basic packages; created empty pages

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Login: React.FC = () => {
+  return (
+    <Container>
+      Test
+    </Container>
+  )
+
+}
+
+export default Login;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      Test
+    </Container>
+  )
+
+}
+
+export default SignUp;
